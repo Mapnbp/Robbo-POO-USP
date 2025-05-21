497dd1a4f3b7f25dc678004325ba745fe63cb918
j-graziosi <104801904+j-graziosi@users.noreply.github.com>
2025-05-21 17:40:17 -0300

    Tirado mouse

---
+++ PrototipoProjetoPOO2025/src/Controler/ControleDeJogo.java
@@ -0,0 +1,52 @@
+package Controler;
+
+import Modelo.Chaser;
+import Modelo.Personagem;
+import Modelo.Hero;
+import auxiliar.Posicao;
+import java.util.ArrayList;
+
+public class ControleDeJogo {
+    
+    public void desenhaTudo(ArrayList<Personagem> e) {
+        for (int i = 0; i < e.size(); i++) {
+            e.get(i).autoDesenho();
+        }
+    }
+    
+    public void processaTudo(ArrayList<Personagem> umaFase) {
+        Hero hero = (Hero) umaFase.get(0);
+        Personagem pIesimoPersonagem;
+        for (int i = 1; i < umaFase.size(); i++) {
+            pIesimoPersonagem = umaFase.get(i);
+            if (hero.getPosicao().igual(pIesimoPersonagem.getPosicao())) {
+                /*TO-DO: verificar se o personagem eh mortal antes de retirar*/
+                if (pIesimoPersonagem.isbTransponivel()) {
+                    if (pIesimoPersonagem.isbMortal())
+                    umaFase.remove(pIesimoPersonagem);
+                }
+            }
+        }
+        for (int i = 1; i < umaFase.size(); i++) {
+            pIesimoPersonagem = umaFase.get(i);
+            if (pIesimoPersonagem instanceof Chaser) {
+                ((Chaser) pIesimoPersonagem).computeDirection(hero.getPosicao());
+            }
+        }
+    }
+
+    /*Retorna true se a posicao p é válida para Hero 
+    com relacao a todos os personagens no array*/
+    public boolean ehPosicaoValida(ArrayList<Personagem> umaFase, Posicao p) {
+        Personagem pIesimoPersonagem;
+        for (int i = 1; i < umaFase.size(); i++) {
+            pIesimoPersonagem = umaFase.get(i);
+            if (!pIesimoPersonagem.isbTransponivel()) {
+                if (pIesimoPersonagem.getPosicao().igual(p)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
