26be24b557bce49d53328a5848542ec50598eb2e
Matheus Araujo Pinheiro <165668357+Mapnbp@users.noreply.github.com>
2025-06-03 22:53:31 -0300

    Delete src directory

--- src/Controler/IPersonagemFactory.java
+++
@@ -1,8 +0,0 @@
-package Controler;
-
-import Modelo.Personagem;
-import java.util.Properties;
-public interface IPersonagemFactory {
-    // Agora o PersonagemFactory precisa do nome da classe para instanciar dinamicamente.
-    Personagem criarPersonagem(String className, String imageName, Properties config);
-}
